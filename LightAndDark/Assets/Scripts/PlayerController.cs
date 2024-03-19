522cf20494a822f1a78d5221a4dd41468d402048
JohnnyR <joraguine99@gmail.com>
2024-03-19 04:08:02 -0700

    fixed player animations

--- LightAndDark/Assets/Scripts/PlayerController.cs
+++ LightAndDark/Assets/Scripts/PlayerController.cs
@@ -1,55 +1,71 @@
-using System;
+using System.Xml.Schema;
+using TMPro;
 using Unity.Netcode;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 
 public class PlayerController : NetworkBehaviour
 {
-
-    public float moveSpeed = 5f;
-    public float jumpForce = 3f;
-    public bool isGrounded;
+    [SerializeField] private float speed = 3f;
+    [SerializeField] float jumpForce = 3f;
+    [SerializeField] private int score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private float _horizontalInput;
+    private Vector2 horizontalMovement;
     private Rigidbody2D rb;
-    public Animator animator;
+    private Animator animator;
     private SpriteRenderer sr;
-    private bool canJump = false;
-    public override void OnNetworkSpawn()
-    {
-        if(!IsOwner) Destroy(this);
-    }
+    private bool isFacingRight = true;
+    public bool isGrounded;
+    public bool canJump;
+    
+    
 
-    private void Awake()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
-        isGrounded = false;
-
     }
-
+    
+    
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        Vector2 horizontalMovement = new Vector2(horizontalInput, 0f) * moveSpeed;
-        animator.SetFloat("Speed",Mathf.Abs(horizontalInput * moveSpeed));
-        if (horizontalInput > 0)
+        if (!IsOwner)
         {
-            sr.flipX = false;
+            return;
         }
         else
         {
-            sr.flipX = true;
+            _horizontalInput = Input.GetAxis("Horizontal") * speed;
+            horizontalMovement = new Vector2(Input.GetAxis("Horizontal"), 0) * speed;
+            rb.velocity = new Vector2(horizontalMovement.x, rb.velocity.y);
         }
-        // Apply horizontal movement
-        rb.velocity = new Vector2(horizontalMovement.x, rb.velocity.y);
-        
+
+        animator.SetFloat("Speed", Mathf.Abs(_horizontalInput));
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            animator.SetBool("isGrounded", true);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);   
+        }
+
+        animator.SetBool("canJump", canJump);
+        // Flip sprite if moving left
+        if (_horizontalInput < 0)
+        {
+            isFacingRight = true;
+            Flip(isFacingRight);
+        }
+        // Flip sprite if moving right
+        else if (_horizontalInput > 0)
+        {
+            isFacingRight = false;
+            Flip(isFacingRight);
         }
     }
     
-    
-    private void OnTriggerEnter2D(Collider2D other)
+    /*private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("circle"))
         {
@@ -57,18 +73,17 @@ public class PlayerController : NetworkBehaviour
             ObjectSpawner.Instance.DespawnObjectServerRpc(networkObject.NetworkObjectId);
             Debug.Log("touching circle");
         }
-
-    }
+    }*/
     
-
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("ground"))
         {
             isGrounded = true;
-
+            canJump = false;
             Debug.Log("touching ground!");
-            animator.SetBool("isGrounded", false);
+
         }
     }
 
@@ -77,7 +92,37 @@ public class PlayerController : NetworkBehaviour
         if (other.gameObject.CompareTag("ground"))
         {
             isGrounded = false;
+            canJump = true;
             Debug.Log("jumping");
+
         }
     }
+    public void IncreaseScore(int amount)
+    {
+        if (!IsOwner) return; // Ensure only the owner can increase the score
+        score += amount;
+        scoreText.text = ("Score: " + score.ToString());
+    }
+    public void Flip(bool isFacingRight)
+    {
+        if (!IsLocalPlayer) return;
+
+        sr.flipX = isFacingRight;
+        FlipSpriteServerRpc(isFacingRight);
+    }
+    [ServerRpc]
+    public void FlipSpriteServerRpc(bool flip)
+    {
+        sr.flipX = flip;
+        FlipSpriteClientRpc(flip);
+    }
+
+    [ClientRpc]
+    public void FlipSpriteClientRpc(bool flip)
+    {
+        if (IsLocalPlayer) return;
+        sr.flipX = flip;
+    }
+
+    
 }
--- LightAndDark/Assets/Scripts/elementController.cs
+++ LightAndDark/Assets/Scripts/elementController.cs
@@ -1,11 +1,14 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
 public class elementController : NetworkBehaviour
 {
-
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!col.CompareTag("Player")) return;
+        if (!NetworkManager.Singleton.IsServer) return;
+        
+    }
 }
